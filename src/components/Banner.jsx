1dd41f6fe0ff5e51d316f8867994e03377003e95
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-03-16 21:38:12 -0500

    GA6 complete

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import "./styling.css"
+
+function Banner() {
+  return (
+    <>
+      <h1>Orbit Report!</h1>
+      <p>Click on the buttons to see the satellites in that orbit type</p>
+    </>
+  );
+}
+
+export default Banner;
